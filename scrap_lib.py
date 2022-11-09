d16ad170031f30e341673206897f0db75a86bff0
Lusifer <vardostafrood@gmail.com>
2022-11-09 07:13:32 +0300

    Add fake user agent

--- scrap_lib.py
+++ scrap_lib.py
@@ -1,8 +1,11 @@
 import requests
+from fake_useragent import UserAgent
 from bs4 import BeautifulSoup as BS
 
 
-def get_page_soup(url) -> BS:
-	response = requests.get(url)
+def get_page_soup(url: str, ua: UserAgent = UserAgent()) -> BS:
+	headers = requests.utils.default_headers()
+	headers.update({'User-Agent': ua.random})
+	response = requests.get(url, headers=headers)
 	return BS(response.text, 'lxml')
 
