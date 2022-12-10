b7d03f176c2420a1f880744083639a9a26965fb2
Lusifer <vardostafrood@gmail.com>
2022-12-10 21:02:40 +0300

    Timeouts logging etc

--- scrap_lib.py
+++ scrap_lib.py
@@ -12,7 +12,8 @@ import logging
 logging.basicConfig(filename='scraper.log',
 			filemode='a',
 			format='%(asctime)s - %(message)s',
-			datefmt='%H:%M:%S')
+			datefmt='%H:%M:%S',
+			level=logging.INFO)
 
 HEADERS = requests.utils.default_headers()
 HEADERS.update({"User-Agent": UserAgent().random})
@@ -63,7 +64,7 @@ def get_page_soup(url: str, headers: dict = HEADERS, use_hd: bool = True,
 def get_from_free_proxy_list() -> dict:
 	proxies = {'http': [], 'https': []}
 	url = "https://free-proxy-list.net/"
-	page = get_page_soup(url, sleep_time=0.1)
+	page = get_page_soup(url, sleep_time=0.1, use_hd=False)
 	table = page.select('.table-striped')[0]
 	trs = table.find_all('tr')
 	ips = [tr.find_all('td') for tr in trs[1:]]
@@ -108,10 +109,10 @@ def get_hidemy_page_links(soup: BeautifulSoup) -> list:
 def get_from_hidemy() -> dict:
 	proxies = {'http': [], 'https': []}
 	url = 'https://hidemy.name/ru/proxy-list/?type=hs&anon=34#list'
-	page = get_page_soup(url, sleep_time=0.1)
+	page = get_page_soup(url, sleep_time=0.1, use_hd=False)
 	urls = get_hidemy_page_links(page)
 	for url in urls:
-		page = get_page_soup(url, sleep_time=0.1)
+		page = get_page_soup(url, sleep_time=0.1, use_hd=False)
 		pp = get_from_one_hidemy_page(page)
 		proxies['http'] += pp['http']
 		proxies['https'] += pp['https']
@@ -123,6 +124,18 @@ def get_all_proxies() -> dict:
 	hmy_pl = get_from_hidemy()
 	proxies = {'http': fpl_pl['http'] + hmy_pl['http'],
 			'https': fpl_pl['https'] + hmy_pl['https']}
+	verified = []
+	for proxy in proxies['https']:
+		try:
+			proxies = {'http': proxy, 'https': proxy}
+			response = requests.get('https://api.myip.com', timeout=10)
+			if response.status_code == 200:
+				verified.append(proxy)
+				logging.info('Verified ' + proxy)
+			sleep(0.5)
+		except Exception as e:
+			pass
+	proxies['https'] = verified
 	return proxies
 
 
@@ -140,9 +153,17 @@ class Requester:
 	sleep_time = 1
 	ua = UserAgent()
 	enable_proxy = False
-	max_requests_url = 3
+	max_requests_url = 99
+	max_unverified_requests = 2
+	refill_proxy_list = 1
+	rotation = 5
+	timeout = 3
+	verify = True
+	del_proxy_strategy = True
+	min_timeout_strategy = True
 	proxies = {}
 	proxy_list = {}
+	verified_proxy_list = []
 
 	def __init__(self, cache_dir: str):
 		self.cache_dir = cache_dir
@@ -180,29 +201,61 @@ class Requester:
 		self.proxies = {'http': random.choice(self.proxy_list['http']),
 				'https': random.choice(self.proxy_list['https'])}
 
+		if len(self.proxy_list['https']) <= self.rotation:
+			for proxy in self.verified_proxy_list:
+				if proxy not in self.proxy_list['https']:
+					self.proxy_list['https'].append(proxy)
+			self.verified_proxy_list = []
+
+		if len(self.proxy_list['https']) <= self.refill_proxy_list:
+			proxy_list = get_all_proxies()
+			self.use_proxies(proxy_list=proxy_list)
+
+	def change_to_verified_proxies(self):
+		if not self.verified_proxy_list:
+			self.change_proxies()
+		else:
+			verified_proxy = random.choice(self.verified_proxy_list)
+			self.proxies = {'http': verified_proxy,
+                                	'https': verified_proxy}
+
+	def delete_proxy_from_list(self, type: str):
+		proxy = self.proxies[type]
+		self.proxy_list[type] = [p for p in self.proxy_list[type] if p != proxy]
+		logging.info('Delete proxy ' + proxy + ' https n: ' + str(
+						len(self.proxy_list['https'])))
+
 	def get_response(self, url: str, req: int = 0):
 		self.change_headers()
-		self.change_proxies()
+		if req < self.max_unverified_requests:
+			self.change_proxies()
+		else:
+			self.change_to_verified_proxies()
+
 		if self.enable_proxy:
 			try:
 				response = requests.get(url, headers=self.headers,
-					proxies=self.proxies)
+					proxies=self.proxies, timeout=self.timeout,
+					verify=self.verify)
 			except Exception as e:
-				response = self.request(url=url, req=req)
+				type = url.split('://')[0]
+				if self.del_proxy_strategy:
+					self.delete_proxy_from_list(type=type)
+				response = self.request(url=url, req=req+1)
 		else:
-			response = requests.get(url, headers=self.headers)
+			response = requests.get(url, headers=self.headers,
+					timeout=self.timeout, verify=self.verify)
 		return response
 
 	def request(self, url: str, req: int = 0):
-		req += 1
 		if req == self.max_requests_url:
-			logging.info('MaxRequestsUrl {req}')
+			logging.info(f"MaxRequestsUrl {req}")
 			raise MaxRequestsUrl
 		if self.sleep_rand:
 			sleep(round(1/random.random(), 1))
 		else:
 			sleep(self.sleep_time)
-		logging.info(str(req + 1) + ' get ' + url)
+		logging.info(str(req+1) + ' get ' + url)
 		return self.get_response(url=url, req=req)
 
 	def get_page_soup(self, url: str) -> BeautifulSoup:
@@ -215,6 +268,8 @@ class Requester:
 			if response.status_code != 200:
 				logging.info(f"StatusCode {response.status_code}")
 				raise ResponseBadStatusCode
+			elif self.proxies not in self.verified_proxy_list:
+				self.verified_proxy_list.append(self.proxies['https'])
 			page_source = response.text
 			if self.use_hd:
 				self.save_on_hd(url=url, page_source=page_source)
